68de2f454d829917f8dd4aaa0d9cf46bc94cd356
nahtand <nathando13403@gmail.com>
2023-10-13 11:03:28 -0700

    completed initialization of philosophers

--- DiningServerImpl.java
+++ DiningServerImpl.java
@@ -12,4 +12,27 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class DiningServerImpl implements DiningServer {
 	// Your code here
+
+	//the different states that the philosophers can be in
+	private enum states {
+		THINKING, EATING, HUNGY
+	}
+
+	//array of 5 represents each philosopher and their current state
+	private states[] philState = new states[5];
+
+	public DiningServerImpl() {
+		//puts all the philosophers into a thinking state
+		for (int i = 0; i < 5; i++) {
+			philState[i] = states.THINKING;
+		}
+	}
+	
+	public void takeForks(int philNumber) {
+		
+	}
+
+	public void returnForks(int philNumber) {
+
+	}
 }
