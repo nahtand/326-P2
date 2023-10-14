e0c21ed56f729e442a554187fe564788bcb90332
nahtand <nathando13403@gmail.com>
2023-10-14 01:01:39 -0700

    made some updates

--- DiningServerImpl.java
+++ DiningServerImpl.java
@@ -21,25 +21,46 @@ public class DiningServerImpl implements DiningServer {
 	//array of 5 represents each philosopher and their current state
 	private states[] philState = new states[5];
 
+	private final Lock lock = new ReentrantLock();
+
 	public DiningServerImpl() {
-		//puts all the philosophers into a thinking state
+		//puts all the philosophers /into a thinking state
+		//initializes the condition(?)
 		for (int i = 0; i < 5; i++) {
 			philState[i] = states.THINKING;
+			self[i] = lock.newCondition();
 		}
 	}
 	
 	//lets given philosopher start eating if it passes test conditions
 	public void takeForks(int philNumber) {
+		lock.lock();
+
 		philState[philNumber] = states.HUNGY;
+		System.out.println("philosopher " + philNumber + " is soooooo hungy");
+
 		//test to see if philosopher's neighbors are eating
 		test(philNumber);
-		if (philState[philNumber] != states.EATING)
-			self[philNumber].wait(); // bruh
+
+		//not eating, goes into wait
+		if (philState[philNumber] != states.EATING){
+			try {
+				self[philNumber].await();
+			} catch (InterruptedException e){
+				System.err.println("interrupted out of wait");
+			}
+		}
+
+			
 	}
 
 	//puts philosopher back into a thinking state, tests other neighboring philosophers
 	public void returnForks(int philNumber) {
+
+		//goes into thinking state
 		philState[philNumber] = states.THINKING;
+		System.out.println("philosopher " + philNumber + " is thinky, hmmmmmm");
+
 		// test neighboring philosophers and lets them eat if conditions are met
 		test((philNumber + 1) % 5); // left
 		test((philNumber + 4) % 5); // right
@@ -47,12 +68,25 @@ public class DiningServerImpl implements DiningServer {
 
 	//checks neighboring philosophers' states and lets the current philospher start eating
 	public void test(int philNumber) {
+		lock.lock();
 		if (philState[(philNumber + 1) % 5] != states.EATING && // Checks the left philosopher
 				philState[(philNumber + 4) % 5] != states.EATING && // Checks the right philosopher 
 				philState[philNumber] == states.HUNGY) {
 					// philosopher starts eating, yummy
 					philState[philNumber] = states.EATING;
+					System.out.println("philosopher " + philNumber + "eating, yummmmm");
+
 					self[philNumber].signal(); //idk wtf the self and signal stuff it for TODO: figure dis shit out
 				}
 	}
+
+	public static void main(String args[]) {
+		//TODO: figure out threading stuff, get this thing tested asap
+			DiningServerImpl server = new DiningServerImpl();
+			for (int i = 0; i < 5; i++){
+				server.takeForks(i);
+			}
+			
+	}
 }
+
